484cf5d373b325b65d22e229b2a66d0eafc6086e
MohammedThouseefM <mthouseef100@gmail.com>
2025-12-17 13:44:57 +0530

    feat: Add authentication middleware to post creation, like, and comment routes.

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { getPosts, createPost, toggleLike, addComment, getComments } = require('../controllers/postController');
 
+const { protect } = require('../middleware/authMiddleware');
+
 router.get('/', getPosts);
-router.post('/', createPost);
-router.post('/:id/like', toggleLike);
-router.post('/:id/comments', addComment);
+router.post('/', protect, createPost);
+router.post('/:id/like', protect, toggleLike);
+router.post('/:id/comments', protect, addComment);
 router.get('/:id/comments', getComments);
 
 module.exports = router;
