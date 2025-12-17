19862ae5fd7a74ea8f3442ba303c2699313acbbb
MohammedThouseefM <mthouseef100@gmail.com>
2025-12-17 13:51:57 +0530

    feat: add authentication protection to notification routes

--- routes/notifications.js
+++ routes/notifications.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getNotifications, markRead } = require('../controllers/notificationController');
 
-router.get('/', getNotifications);
-router.put('/:id/read', markRead);
+const { protect } = require('../middleware/authMiddleware');
+
+router.get('/', protect, getNotifications);
+router.put('/:id/read', protect, markRead);
 
 module.exports = router;
